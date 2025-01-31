ecf874547b5b6b129cdda3d29b0135e1f810e03e
Prom004 <izerepromise004@gmail.com>
2025-01-31 16:24:15 +0200

    sliding menu bar

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const sideMenu = document.getElementById("sideMenu");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.classList.remove("translate-x-full");
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.classList.add("translate-x-full");
+});
